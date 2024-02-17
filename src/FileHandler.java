a26f9c8f0845f1dbb22fea7445501b2d74483842
Randy Modglin <RJMJR105@gmail.com>
2024-02-17 17:01:01 -0500

    Cleaning up folders
    Creating, Printing, and Deleting in the interface

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -4,65 +4,68 @@ import java.io.File;
 import java.io.IOException;
 
 public class FileHandler {
-    public void createCSV(String filename) {
-        try {
-            String folderName = "CSVs";
 
-            // Create the folder path in the same directory as program
-            String folderPath = System.getProperty("user.dir") + File.separator + folderName;
-            File folder = new File(folderPath);
-            if (folder.mkdirs())
-                System.out.println("Folder Created: " + folderPath);
+	public void createCSV(String filename) {
+		try {
+			String folderName = "CSVs";
 
-            // Place the CSV file into the CSVs folder
-            String filePath = folderPath + File.separator + filename + ".csv";
-            File csv = new File(filePath);
+			// Create the folder path in the same directory as program
+			String folderPath = System.getProperty("user.dir") + File.separator + folderName;
+			File folder = new File(folderPath);
+			if (folder.mkdirs()) {
+				System.out.println("Folder Created: " + folderPath);
+			}
 
-            // Create the CSV file, check if name already exists
-            if (csv.createNewFile())
-                System.out.println("File created: " + csv.getName());
-            else
-                System.out.println("File exists already: " + csv.getName());
+			// Place the CSV file into the CSVs folder
+			String filePath = folderPath + File.separator + filename + ".csv";
+			File csv = new File(filePath);
 
-        } catch (IOException e) {
-            System.err.print("Error: ");
-            e.printStackTrace();
-        }
-    }
+			// Create the CSV file, check if name already exists
+			if (csv.createNewFile()) {
+				System.out.println("File created: " + csv.getName());
+			} else {
+				System.out.println("File exists already: " + csv.getName());
+			}
 
-    public void deleteCSV(String filename) {
-        try {
-            // Get the path for CSV folder
-            String filePath = System.getProperty("user.dir") + File.separator + "CSVs" + File.separator + filename + ".csv";
-            File csvFile = new File(filePath);
+		} catch (IOException e) {
+			System.err.print("Error: ");
+			e.printStackTrace();
+		}
+	}
 
-            // Check if the file exists, delete if it does
-            if (csvFile.exists()) {
-                if (csvFile.delete()) {
-                    System.out.println("File deleted: " + csvFile.getName());
-                } else {
-                    System.out.println("Failed to delete file: " + csvFile.getName());
-                }
-            } else {
-                System.out.println("File does not exist: " + csvFile.getName());
-            }
-        } catch (Exception e) {
-            System.err.println("Error occurred while trying to delete CSV file named:" + filename);
-            e.printStackTrace();
-        }
-    }
+	public void deleteCSV(String filename) {
+		try {
+			// Get the path for CSV folder
+			String filePath = System.getProperty("user.dir") + File.separator + "CSVs" + File.separator + filename
+					+ ".csv";
+			File csvFile = new File(filePath);
 
-    public File getCSVFile(String filename) {
-        // Get the path for CSV folder
-        String filePath = System.getProperty("user.dir") + File.separator + "CSVs" + File.separator + filename + ".csv";
-        File csvFile = new File(filePath);
+			// Check if the file exists, delete if it does
+			if (csvFile.exists()) {
+				if (csvFile.delete()) {
+					System.out.println("File deleted: " + csvFile.getName());
+				} else {
+					System.out.println("Failed to delete file: " + csvFile.getName());
+				}
+			} else {
+				System.out.println("File does not exist: " + csvFile.getName());
+			}
+		} catch (Exception e) {
+			System.err.println("Error occurred while trying to delete CSV file named:" + filename);
+			e.printStackTrace();
+		}
+	}
 
-        // Check if the file exists, return it if it does
-        if (csvFile.exists()) {
-            return csvFile;
-        } else {
-            System.out.println("File does not exist: " + csvFile.getName());
-            return null;
-        }
-    }
+	public File getCSVFile(String filename) {
+		// Get the path for CSV folder
+		String filePath = System.getProperty("user.dir") + File.separator + "CSVs" + File.separator + filename + ".csv";
+		File csvFile = new File(filePath);
+
+		// Check if the file exists, return it if it does
+		if (csvFile.exists()) {
+			return csvFile;
+		} else {
+			return null;
+		}
+	}
 }
--- src/LibraryInterface.java
+++ src/LibraryInterface.java
@@ -1,4 +1,51 @@
 package src;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class LibraryInterface {
+
+	private final FileHandler handler = new FileHandler();
+	private final FileEditor editor = new FileEditor();
+
+	public void createCSV(String filename) {
+		File currFile = handler.getCSVFile(filename);
+
+		if (currFile == null) {
+			handler.createCSV(filename);
+			System.out.println("Created File: " + filename);
+		} else {
+			System.out.println("File already exists");
+		}
+	}
+
+	public void printCSV(String filename) {
+		File currFile = handler.getCSVFile(filename);
+
+		if (currFile != null) {
+			try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+				String line;
+				while ((line = reader.readLine()) != null) {
+					System.out.println(line);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("File does not exist");
+		}
+	}
+
+	public void deleteCSV(String filename) {
+		File currFile = handler.getCSVFile(filename);
+
+		if (currFile != null) {
+			handler.deleteCSV(filename);
+			System.out.println("Deleted File: " + filename);
+		} else {
+			System.out.println("File doesn't exists");
+		}
+	}
 }
